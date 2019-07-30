e54eeec62ee2938dc7a5dff251d9cdec0a32379d
Rawann Silva <rawannsilveira@gmail.com>
2019-07-29 22:18:51 -0300

    Seção 121 do Curso C# Finalizada

---
+++ CourseSeção121/Entities/Comment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseSeção121.Entities
+{
+    class Comment
+    {
+
+        public string Text { get; set; }
+
+        public Comment()
+        {
+
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
---
+++ CourseSeção121/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseSeção121.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+        Pending_payment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
